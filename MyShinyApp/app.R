ccd8e6a6c0f913c9ccab6dbff9d729d034058964
dawn-cheung <144048585+dawn-cheung@users.noreply.github.com>
2023-11-15 16:36:47 +0800

    i am. in tears.

--- MyShinyApp/app.R
+++ MyShinyApp/app.R
@@ -5,109 +5,133 @@ library(dplyr)
 library(countrycode)
 library(shiny)
 library(ggpubr)
+library(plotly)
 
+# Import the data with coordinates
 # Import the data with coordinates
 world_map <- map_data("world")
 world_map$region <- countrycode(world_map$region, "country.name", "iso3c")
+ISOCODESAREGGFKINGKILLME <- c("iso3") #keeps track of what iso code we're on and im so sorry i couldnt be bothered to figure out how to tag it onto word_map itself
 accesstonet <- read.csv("accesstointernet.csv")
 netspeed <- read.csv("worldwideInternetSpeed.csv") #ISO2 code used in dataset!
 netprice <- read.csv("worldwideMobileDataPricing2019to2023.csv") #ISO2 code used in dataset!
 
-# Data cleaning functions
-clean_data <- function(dataset, year_col, value_cols) {
-  dataset %>%
-    pivot_longer(
-      cols = value_cols,
-      names_to = year_col,
-      values_to = "values"
-    ) %>%
-    rename(region = Country.Code)
+#data cleaning for access to net
+access_dataset <- accesstonet %>%
+  pivot_longer(
+    cols = D1990:D2022,
+    names_to = "Dyear",
+    values_to = "values") %>%
+  rename(region = Country.Code)
+
+#data cleaning for speed of net
+speed_dataset <- netspeed %>%
+  pivot_longer(
+    cols = D2017:D2023,
+    names_to = "Dyear",
+    values_to = "values") %>%
+  rename(region = Country.Code)
+
+#data cleaning for price of net
+price_dataset <- netprice %>% 
+  pivot_longer(
+    cols = D2019:D2023,
+    names_to = "Dyear",
+    values_to = "values") %>%
+  rename(region = Country.Code)
+
+price_dataset$values <- gsub('[$,]', '', price_dataset$values) 
+world_map$region <- countrycode(world_map$region, "iso3c", "iso2c") #RESET ISO CODES
+ISOCODESAREGGFKINGKILLME <- "iso2c"
+
+#group = group connects the points in the correct order
+mapplotting <- function(wee, dayear, colourscheme) {
+  if(identical(wee, access_dataset)) { 
+    if (ISOCODESAREGGFKINGKILLME == "iso2c") { #its a global variable so its fine
+      world_map$region <- countrycode(world_map$region, "iso2c", "iso3c")
+      ISOCODESAREGGFKINGKILLME == "iso3c"
+    } else {
+      ISOCODESAREGGFKINGKILLME == "iso3c"
+    }
+  } else {
+    if (ISOCODESAREGGFKINGKILLME == "iso3c") {
+      world_map$region <- countrycode(world_map$region, "iso3c", "iso2c")
+      ISOCODESAREGGFKINGKILLME == "iso2c"
+    } else {
+      ISOCODESAREGGFKINGKILLME == "iso3c"
+    }
+  }
+  weep <- wee %>% filter(Dyear == paste0("D", dayear))
+  weep$values <- as.numeric(weep$values)
+  mapdataset <- left_join(world_map, weep, by = "region")
+  ggplot(mapdataset, aes(long, lat, group = group)) +
+    geom_polygon(aes(fill = values), color = "white", size = 0.04) +
+    scale_fill_viridis_c(option = colourscheme) +
+    theme(plot.margin = margin(0, 0, 0, 0, "cm"))
 }
 
-# Clean access data
-access_dataset <- clean_data(accesstonet, "Xyear", "X1990:X2022")
-
-# Clean speed data
-speed_dataset <- clean_data(netspeed, "Dyear", "D2017:D2023")
-
-# Clean price data
-price_dataset <- clean_data(netprice, "Dyear", "D2019:D2023")
-price_dataset$values <- as.numeric(gsub('[$,]', '', price_dataset$values))
-
-# Shiny app
 ui <- fluidPage(
-  titlePanel("Worldwide Internet Data"),
+  
+  # App title ----
+  titlePanel("Comparing Variations in Access, Speed, and Price of Internet, Over Time"),
+  
+  # Sidebar layout with a input and output definitions ----
   sidebarLayout(
+    
+    # Sidebar panel for inputs ----
     sidebarPanel(
-      selectInput("yearInput", "Select Year", choices = unique(access_dataset$Xyear))
+      
+      # Input: Selector for choosing dataset ----
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("ACCESS: Individuals using the Internet (% of population)", "SPEED: Mean download speed (Mbps)", "PRICE: Average price of 1GB (in USD)")),
+      
+      sliderInput(inputId = "year",
+                  label = "Choose the year:",
+                  min = 1990, max = 2022, value = 2020, sep = ""),
     ),
+    
+    # Main panel for displaying outputs ----
     mainPanel(
-      plotOutput("mapPlot")
+      
+      # Output: Verbatim text for data summary ----
+      verbatimTextOutput("summary"),
+      
+      # Output: HTML table with requested number of observations ----
+      plotlyOutput("view", width = "80%")
     )
   )
 )
-
-server <- function(input, output) {
-  observe({
-    accessyear <- access_dataset %>% filter(Xyear == input$yearInput)
-    sostupid_map <- left_join(world_map, accessyear, by = "region")
-    
-    speedyear <- speed_dataset %>% filter(Dyear == input$yearInput)
-    sohungry_map <- left_join(world_map, speedyear, by = "region")
-    
-    priceyear <- price_dataset %>% filter(Dyear == input$yearInput)
-    sotired_map <- left_join(world_map, priceyear, by = "region")
-    
-    sostupid_map$values <- as.numeric(sostupid_map$values)
-    sohungry_map$values <- as.numeric(sohungry_map$values)
-    sotired_map$values <- as.numeric(sotired_map$values)
-    
-    output$mapPlot <- renderPlot({
-      par(mfrow = c(1, 3), mar = c(2, 2, 2, 2))
-      mapplotting(sostupid_map, sostupid_map$values, "B")
-      mapplotting(sohungry_map, sohungry_map$values, "C")
-      mapplotting(sotired_map, sotired_map$values, "C")
-    })
+server <- function(input, output, session) {
+  
+  # Return the requested dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "ACCESS: Individuals using the Internet (% of population)" = access_dataset,
+           "PRICE: Average price of 1GB (in USD)" = price_dataset, #2019-2023
+           "SPEED: Mean download speed (Mbps)" = speed_dataset) #2017 to 2023
   })
-}
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  observe({
+    if (identical(datasetInput(),access_dataset)) {
+      minnie = 1990L
+      maxxie = 2022L
+    } else if (identical(datasetInput(),price_dataset)) {
+      minnie = 2019L
+      maxxie = 2023L
+    } else if (identical(datasetInput(),speed_dataset)){
+      minnie = 2017L
+      maxxie = 2023L
+    }
+    updateSliderInput(
+      session = session,
+      inputId = "year",
+      min = minnie,
+      max = maxxie
     )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  })
+  output$view <- renderPlotly({
+    ggplotly(mapplotting(datasetInput(), as.character(input$year), "B"))
+  })
+  
 }
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
